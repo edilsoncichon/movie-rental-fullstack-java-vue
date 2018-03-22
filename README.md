c3438aef15af50039cf76f83950742ea95f56749
Edilson Cichon <edilsoncichon@hotmail.com>
2018-03-22 17:23:07 -0300

    Added README for project root and url api adjusted.

---
+++ README.md
@@ -0,0 +1,3 @@
+# WebService API (JAVA) + Client (Vue.js)
+
+TODO Especificação dos passos pra usar esse projeto.
--- client/src/services/Http.js
+++ client/src/services/Http.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const client = axios.create({
-  baseURL: 'http://localhost:8080/Locadora',
+  baseURL: 'http://localhost:8080/api',
   timeout: '1000',
   headers: {
     // 'X-Custom-Header': 'true'
