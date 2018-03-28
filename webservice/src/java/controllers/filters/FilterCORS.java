6d83c48cd87993cc455e3bd4a84632850b6ccb40
Edilson Cichon <edilsoncichon@hotmail.com>
2018-03-28 19:32:22 -0300

    Filter added: allow CORS and Content Type set JSON default.

---
+++ webservice/src/java/controllers/filters/FilterCORS.java
@@ -0,0 +1,40 @@
+package controllers.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/* @ WebFilter(asyncSupported = true, urlPatterns = { "/*" }) */
+public class FilterCORS implements Filter {
+ 
+    @Override
+    public void doFilter(ServletRequest sReq, ServletResponse sRes, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) sReq;
+        HttpServletResponse res = (HttpServletResponse) sRes;
+        System.out.println("FilterCORS HTTP Request: " + req.getMethod());
+ 
+        res.addHeader("Access-Control-Allow-Origin", "*");
+        res.addHeader("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD, PUT, DELETE, POST");
+ 
+        // For HTTP OPTIONS verb/method reply with ACCEPTED status code -- per CORS handshake
+        if (req.getMethod().equals("OPTIONS")) {
+            res.setStatus(HttpServletResponse.SC_ACCEPTED);
+            return;
+        }
+        // pass the request along the filter chain
+        chain.doFilter(sReq, sRes);
+    }
+ 
+    @Override
+    public void init(FilterConfig fConfig) throws ServletException {}
+    
+    @Override
+    public void destroy() {}
+}
---
+++ webservice/src/java/controllers/filters/FilterContentType.java
@@ -0,0 +1,27 @@
+package controllers.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.ServletException;
+ 
+/* @ WebFilter(asyncSupported = true, urlPatterns = { "/*" }) */
+public class FilterContentType implements Filter {
+ 
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        res.setContentType("application/json;charset=UTF-8");
+        chain.doFilter(req, res);
+    }
+ 
+    @Override
+    public void init(FilterConfig fConfig) throws ServletException {}
+    
+    @Override
+    public void destroy() {}
+    
+}
