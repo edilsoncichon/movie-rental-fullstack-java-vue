4ce7d756e5019851a7ca5f349aa11239715b284a
Edilson Cichon <edilsoncichon@hotmail.com>
2018-03-28 19:44:42 -0300

    ControllerBase refactoring for standardize calls http and avoid duplication of code in Controllers children.

--- webservice/src/java/controllers/CtlBase.java
+++ webservice/src/java/controllers/CtlBase.java
@@ -1,7 +1,12 @@
 package controllers;
 
+import domains.Domain;
+import java.util.List;
+import javax.json.Json;
 import java.io.IOException;
-import java.io.PrintWriter;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.annotation.WebServlet;
@@ -10,82 +15,88 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "ServletBase", urlPatterns = {"/ServletBase"})
 public abstract class CtlBase extends HttpServlet {
-
-    private void configRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("application/json;charset=UTF-8");
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        this.processRequest(request, response, response.getWriter());
-    }
     
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @param out writer
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected abstract void processRequest(HttpServletRequest request, HttpServletResponse response, PrintWriter out)
-            throws ServletException, IOException;
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
-     * @param request servlet request
-     * @param response servlet response
+     * @param req servlet request
+     * @param res servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
-        configRequest(request, response);
+        res.getWriter().print("Info: [GET] method not implemented for this resource!");
+        res.setStatus(404);
     }
 
     /**
      * Handles the HTTP <code>POST</code> method.
      *
-     * @param request servlet request
-     * @param response servlet response
+     * @param req servlet request
+     * @param res servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
-        configRequest(request, response);
+        res.getWriter().print("Info: [POST] method not implemented for this resource!");
+        res.setStatus(404);
     }
     
     /**
      * Handles the HTTP <code>PUT</code> method.
      *
-     * @param request servlet request
-     * @param response servlet response
+     * @param req servlet request
+     * @param res servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     @Override
-    protected void doPut(HttpServletRequest request, HttpServletResponse response)
+    protected void doPut(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
-        configRequest(request, response);
+        res.getWriter().print("Info: [PUT] method not implemented for this resource!");
+        res.setStatus(404);
     }
     
     /**
      * Handles the HTTP <code>DELETE</code> method.
      *
-     * @param request servlet request
-     * @param response servlet response
+     * @param req servlet request
+     * @param res servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     @Override
-    protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+    protected void doDelete(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
-        configRequest(request, response);
+        res.getWriter().print("Info: [DELETE] method not implemented for this resource!");
+        res.setStatus(404);
     }
-    // </editor-fold>
+    
+    /**
+     * Converte um objeto List de Domains para um JsonArrayBuilder.
+     * 
+     * @param list
+     * @return 
+     */
+    protected JsonArrayBuilder list2Json(List<Domain> list) {
+        JsonArrayBuilder builder = Json.createArrayBuilder();
+        list.forEach(domain -> { builder.add(domain.toJSON()); });
+        return builder;
+    }
+    
+    protected String toJSONResponse(List data) {
+        JsonObject listJson = Json.createObjectBuilder()
+                    .add("data", list2Json(data)).build();
+        return listJson.toString();
+    }
+    
+    protected String toJSONResponse(Domain domain) {
+        JsonObject objJSON = Json.createObjectBuilder()
+                .add("data", domain.toJSON()).build();
+        return objJSON.toString();
+    }
 }
